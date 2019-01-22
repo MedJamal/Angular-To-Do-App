b2b9708cf99c0e6b6c3e8e296dbc5ea3e18a3fe3
Mohamed Jamal OUAZANI TOUHAMI <medjamal.ouazani@gmail.com>
2019-01-22 03:42:46 +0000

    First init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
 
+// declare var require: any;
+import {Md5} from "md5-typescript"
+
+interface Todo {
+  id: string,
+  todo: string,
+  isDone: boolean,
+  createdAt: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todosapp';
+  
+  todoInput: string;
+  todos: Todo[];
+  
+  constructor(){
+    //
+    if(!localStorage.getItem('todos')){
+      localStorage.setItem('todos', JSON.stringify([]));
+    }
+  }
+  
+  ngOnInit(){
+    this.todoInput = '';
+    // Get todos from local storage
+
+    this.todos = JSON.parse(localStorage.getItem('todos'));
+    // alert(this.todos.length)
+  }
+  
+  addTodo() {
+    
+    this.todos.push({
+      id: Md5.init(Date.now()),
+      todo: this.todoInput,
+      isDone: false,
+      createdAt: Date.now()
+    });
+    
+    // Save todo to the local storage
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+    this.todoInput = '';
+    
+  }
+
+
 }
